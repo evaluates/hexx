35c6c9290b39b63695a4da2526fb95999229566d
Andrew Kozin <andrew.kozin@gmail.com>
2015-02-28 14:56:36 +0300

    [FEATURE] Generator class
    The base class for scaffolders. Extends the Thor::Group with
    additional methods.
    
    == Addons
    
    * Hexx::Generator
    
    Private utilities:
    
    * Hexx::Generator::File
    * Hexx::Generator::Folder

--- lib/hexx.rb
+++ lib/hexx.rb
@@ -2,6 +2,7 @@
 require "hexx-suit"
 
 require_relative "../config/initializer"
+require_relative "hexx/generator"
 
 # Collection of scaffolders for projects that follows the Hexagonal architecture
 module Hexx
