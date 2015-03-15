b8f6d685e8b3843fbd857edfa467c6d6b4c32b0d
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-15 14:15:25 +0300

    [VERSION] 9.0.0
    See the previous commit message for details.

--- lib/hexx/version.rb
+++ lib/hexx/version.rb
@@ -1,7 +1,9 @@
 # encoding: utf-8
 
 module Hexx
+
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "8.0.0".freeze
+  VERSION = "9.0.0".freeze
+
 end # module Hexx
