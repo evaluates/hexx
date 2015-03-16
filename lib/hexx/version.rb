e9fe3c0fb2e5895d1b05956dba4c95d2b856c659
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-16 19:21:43 +0300

    [VERSION] v9.0.1
    * dependency updated: hexx-suit 1.2 -> 2.0

--- lib/hexx/version.rb
+++ lib/hexx/version.rb
@@ -4,6 +4,6 @@ module Hexx
 
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "9.0.0".freeze
+  VERSION = "9.0.1".freeze
 
 end # module Hexx
