3fecb66e9bd3c6e5b0cda420e186e62e82006d7a
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-19 08:39:18 +0300

    [FEATURE] $ hexx policy
    The command scaffolds a policy with a specification and translations.

--- spec/tests/bin/hexx_spec.rb
+++ spec/tests/bin/hexx_spec.rb
@@ -1,32 +1,43 @@
 # encoding: utf-8
 require "hexx-validators"
+require "policy/cli"
 
 describe "$ hexx", :sandbox do
 
-  shared_examples "specific scaffolder" do
-    # subject command, its arguments and generator should be defined in context
+  shared_examples "running scaffolder" do |generator|
 
-    let!(:expected_output) do
+    let!(:expected) do
+      prepare_sandbox
       capture_stdout { try_in_sandbox { generator.start(arguments) } }
     end
 
-    let!(:actual_output) do
+    let!(:actual) do
       prepare_sandbox
       try_in_sandbox { return `#{ [:hexx, subject, arguments].join(" ") }` }
     end
 
     it "[starts the generator]" do
-      expect(actual_output).to eq expected_output
+      expect(actual).to eq expected
     end
-  end
+
+  end # shared_examples
 
   context "validator" do
 
     subject { :validator }
 
-    it_behaves_like "specific scaffolder" do
+    it_behaves_like "running scaffolder", Hexx::Validators::Generator do
       let(:arguments) { %w(in_future -e not_in_future -o allow_nil) }
-      let(:generator) { Hexx::Validators::Generator }
+    end
+
+  end # validator
+
+  context "policy" do
+
+    subject { :policy }
+
+    it_behaves_like "running scaffolder", Policy::CLI do
+      let(:arguments) { %w(consistency -a debet credit -l en fr) }
     end
 
   end # validator
