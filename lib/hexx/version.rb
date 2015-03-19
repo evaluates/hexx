4636c757adeb624800798a192a8857f15b55ea9a
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-19 14:47:50 +0300

    [VERSION] 9.1.0

--- lib/hexx/version.rb
+++ lib/hexx/version.rb
@@ -4,6 +4,6 @@ module Hexx
 
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "9.0.1".freeze
+  VERSION = "9.1.0".freeze
 
 end # module Hexx
